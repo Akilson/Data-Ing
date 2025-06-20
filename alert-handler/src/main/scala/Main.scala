c59098bb51c96c008eef683c5af56788b78d175b
asaa0 <angela.saade@epita.fr>
2025-06-20 11:32:41 +0200

    feat(feat-alert-handler): init alert handler

--- alert-handler/src/main/scala/Main.scala
+++ alert-handler/src/main/scala/Main.scala
@@ -1,3 +1,31 @@
+import org.apache.kafka.clients.consumer.{KafkaConsumer, ConsumerConfig, ConsumerRecord, ConsumerRecords}
+import org.apache.kafka.common.serialization.{StringDeserializer}
+import java.util.Properties
+import java.time.Duration
+import scala.collection.JavaConverters._
+import io.circe.parser.parse
+
 object Main extends App {
-  println("Alert Handler started")
+  println("Alert handler started")
+
+  val inputTopic = "alerts"
+  
+  // Consumer Setup
+  val consumerProps: Properties = new Properties()
+  consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
+  consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
+  consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "alert-selector-group")
+
+  val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](consumerProps)
+  consumer.subscribe(java.util.List.of(inputTopic))
+
+  def consumeLoop(): Unit = {
+    val records = consumer.poll(Duration.ofMillis(100))
+    records.asScala.foreach { record =>
+    }
+    consumeLoop()
+  }
+
+  consumeLoop()
 }
