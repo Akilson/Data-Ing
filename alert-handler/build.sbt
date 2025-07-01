99a1449fd8492edf982d211c36e0070c3ac8bd98
baptiste <baptiste.arnold@epita.fr>
2025-07-01 19:32:34 +0200

    feat(feat-alert-handler): reporting by sms + discord messages

--- alert-handler/build.sbt
+++ alert-handler/build.sbt
@@ -4,4 +4,7 @@ libraryDependencies ++= Seq(
   "org.apache.kafka" %% "kafka" % "3.5.1",
   "io.circe" %% "circe-core" % "0.14.7",
   "io.circe" %% "circe-parser" % "0.14.7",
+  "com.twilio.sdk" % "twilio" % "9.8.0",
+  "com.softwaremill.sttp.client3" %% "core" % "3.8.11",
+  "io.circe" %% "circe-generic" % "0.14.5",
 )
--- alert-handler/src/main/scala/Main.scala
+++ alert-handler/src/main/scala/Main.scala
@@ -4,28 +4,147 @@ import java.util.Properties
 import java.time.Duration
 import scala.collection.JavaConverters._
 import io.circe.parser.parse
+import com.twilio.Twilio
+import com.twilio.rest.api.v2010.account.Message
+import com.twilio.`type`.PhoneNumber
+import sttp.client3._
+import sttp.model.MediaType
+import io.circe.generic.auto._
+import io.circe.syntax._
 
 object Main extends App {
   println("Alert handler started")
 
-  val inputTopic = "alerts"
+  object SmsSender {
+    val accountSid = "AC971b8e3db9f34f829879fcc2ba608aab"     // or hardcode for now
+    val authToken = "5d1aa3d5efb7aa7869547f4da7e5f22c"        // or hardcode
+    val fromNumber = "+12518508296"                     // Your Twilio number
+
+    def sendSms(to: String, body: String): Unit = {
+      Twilio.init(accountSid, authToken)
+      val message = Message
+        .creator(
+          new PhoneNumber(to),
+          new PhoneNumber(fromNumber),
+          body
+        )
+        .create()
+
+      println(s"✅ SMS sent! SID: ${message.getSid}")
+    }
+  }
+  // SmsSender.sendSms("+33644777142", "beginning of handler")
+  object DiscordNotifierSev2 {
+
+    // Replace with your Discord webhook URL
+    val webhookUrl = "https://discord.com/api/webhooks/1389656695256514560/VvkjoM-EhZ9QFxIVIYrzrnY2X2aR-UjDzHmKbOG4p130thgGxr4suK1MAe-Qhb0-CNx9"
+
+    case class DiscordMessage(content: String)
+
+    def sendDiscordMessage(message: String): Unit = {
+      val backend = HttpURLConnectionBackend()
+      val discordMsg = DiscordMessage(message)
+
+      val request = basicRequest
+        .post(uri"$webhookUrl")
+        .contentType(MediaType.ApplicationJson)
+        .body(discordMsg.asJson.noSpaces)
+
+      val response = request.send(backend)
+
+      if (response.code.isSuccess) println("Alert sent to Discord!")
+      else println(s"Failed to send Discord message: ${response.code} - ${response.body}")
+    }
+  }
+
+  object DiscordNotifierSev3 {
+
+    // Replace with your Discord webhook URL
+    val webhookUrl = "https://discord.com/api/webhooks/1389659062328758323/BsJiIN-6E0iue0p0pspdW07xrXnD7qhrw7ouMQKINGKa5WzE_aDkjYFZuAVxlEqsan59"
+
+    case class DiscordMessage(content: String)
+
+    def sendDiscordMessage(message: String): Unit = {
+      val backend = HttpURLConnectionBackend()
+      val discordMsg = DiscordMessage(message)
+
+      val request = basicRequest
+        .post(uri"$webhookUrl")
+        .contentType(MediaType.ApplicationJson)
+        .body(discordMsg.asJson.noSpaces)
+
+      val response = request.send(backend)
+
+      if (response.code.isSuccess) println("Alert sent to Discord!")
+      else println(s"Failed to send Discord message: ${response.code} - ${response.body}")
+    }
+  }
+
+  def smsForSev1(message: String): String = {
+    parse(message).toOption.flatMap { json =>
+      val cursor = json.hcursor
+      val temp = cursor.get[Double]("temperature").toOption
+      val level = cursor.get[Int]("tankLevel").toOption
+      val humidity = cursor.get[Double]("humidity").toOption
+      val fermenter = cursor.get[String]("deviceId").toOption
+      (temp, level, humidity, fermenter) match {
+        case (Some(t), Some(l), Some(h), Some(f)) => Some("🔥 SEV1 Alert!: " + f + " - Temp: " + t + "°C, Level: " + l + "%, Humidity: " + h + "%")
+        case _ => Some("")
+      }
+    }.getOrElse("")
+  }
+
+  def smsForSev23(message: String): String = {
+    parse(message).toOption.flatMap { json =>
+      val cursor = json.hcursor
+      val temp = cursor.get[Double]("temperature").toOption
+      val level = cursor.get[Int]("tankLevel").toOption
+      val humidity = cursor.get[Double]("humidity").toOption
+      val fermenter = cursor.get[String]("deviceId").toOption
+      (temp, level, humidity, fermenter) match {
+        case (Some(t), Some(l), Some(h), Some(f)) => Some(f + " - Temp: " + t + "°C, Level: " + l + "%, Humidity: " + h + "%")
+        case _ => Some("")
+      }
+    }.getOrElse("")
+  }
+  val inputTopicSev1 = "alerts-sev1"
+  val inputTopicSev2 = "alerts-sev2"
+  val inputTopicSev3 = "alerts-sev3"
   
   // Consumer Setup
   val consumerProps: Properties = new Properties()
   consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
   consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
   consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
-  consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "alert-selector-group")
+  consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "alert-handler-group")
 
   val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](consumerProps)
-  consumer.subscribe(java.util.List.of(inputTopic))
+  consumer.subscribe(List(inputTopicSev1, inputTopicSev2, inputTopicSev3).asJava)
 
   def consumeLoop(): Unit = {
     val records = consumer.poll(Duration.ofMillis(100))
     records.asScala.foreach { record =>
+      if (record.topic() == inputTopicSev1) {
+        val messageToSend = smsForSev1(record.value())
+        if (messageToSend == "") {
+          println(s"Received SEV1 alert but could not parse: ${record.value()}")
+          return
+        }
+        SmsSender.sendSms("+33644777142", messageToSend) // Replace with actual phone number
+        println(s"Received SEV1 alert: ${record.value()}")
+      } else if (record.topic() == inputTopicSev2) {
+        val messageToSend = "Alert: " + smsForSev23(record.value())
+        DiscordNotifierSev2.sendDiscordMessage(messageToSend)
+        // println(s"Received SEV2 alert: ${record.value()}")
+      } else if (record.topic() == inputTopicSev3) {
+        val messageToSend = "Alert: " + smsForSev23(record.value())
+        DiscordNotifierSev3.sendDiscordMessage(messageToSend)
+        // println(s"Received SEV3 alert: ${record.value()}")
+      } else {
+        println(s"Received unknown alert from topic ${record.topic()}: ${record.value()}")
+      }
     }
     consumeLoop()
   }
-
   consumeLoop()
 }
