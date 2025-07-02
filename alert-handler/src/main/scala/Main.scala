361db1b6215f619b88ef3478bf70e8f91da164bc
milik0 <mili.khaled1@gmail.com>
2025-07-02 19:46:04 +0200

    fix(feat-dockerfiling): docker file the first 3 services, have to fix the creds of alert-handler

--- alert-handler/src/main/scala/Main.scala
+++ alert-handler/src/main/scala/Main.scala
@@ -114,7 +114,7 @@ object Main extends App {
   
   // Consumer Setup
   val consumerProps: Properties = new Properties()
-  consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092")
   consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
   consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
   consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "alert-handler-group")
